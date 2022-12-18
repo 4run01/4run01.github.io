3f5b080287b2b739f49c85d28cf7a8da85e5d3b1
4run01 <117312242+4run01@users.noreply.github.com>
2022-12-19 03:56:54 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,93 @@
+var telegram = "https://t.me/Arun46";
+var snapchat = "https://www.snapchat.com/add/arunrossi01";
+var password = "sudoru";
+var whatsapp = "https://wa.me/qr/J22WSWDMX4W5L1";
+var instagram = "https://www.instagram.com/black_swagger46/";
+var github = "https://github.com/4run01";
+var email = 'mailto:arunofficial0104@gmail.com';
+
+whois = [
+    "<br>",
+    "Hey, I'm Arun!👋",
+    "I'm ethical hacker and Penetration tester, who tests websites which as vulnerability ",
+    "knowledge in IOT and interested in Artificial Intelligence",
+    "After graduating with a Bachelor's in Computer Application, I Joined M.sc cyber forensics and information security",
+    "currenty im working on a future project based on IOT ",
+    "While doing all of that, I started to develop my own website",
+    "NOW Im intersed in doing masters in artificial Intelligence with cyber security",
+    "I found and exploit websites which as vulnerbility ",
+    "Hands on tools related to hacking in wireshark,nmap,FTK,Nikto,Netstumbler,Burpsuite,Metasploit.etc,",
+    "In future planing to make my own company",
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+    "if you think you know me,you are absolutely wrong!",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    'telegram        <a href="' + telegram + '" target="_blank"> telegram/Arun' + "</a>",
+    'snapchat        <a href="' + snapchat + '" target="_blank"> snapchat/Arun' + '</a>',
+    'whatsapp       <a href="' +  whatsapp + '" target="_blank">  whatsapp/Arun' + "</a>",
+    'instagram      <a href="' + instagram + '" target="_blank">  instagram/Arun' + '</a>',
+    'github         <a href="' + github + '" target="_blank">  github/Arun' + "</a>",
+    "<br>"
+];
+
+secret = [
+    "<br>",
+    '<span class="command">sudo</span>           Only use if you\'re admin',
+    "<br>"
+];
+
+projects = [
+    "<br>",
+    "Still curating... most projects are offline, on GitHub, or confidential.",
+    "<br>"
+];
+
+video = [
+    "<br>",
+    "enjoy the video on youtube.",
+    "<br>"
+];
+
+help = [
+    "<br>",
+    '<span class="command">whois</span>          Who is Arun?',
+    '<span class="command">whoami</span>         Who are you?',
+    '<span class="command">video</span>          Opening YouTube',
+    '<span class="command">social</span>         Display social networks',
+    '<span class="command">secret</span>         Find the password',
+    '<span class="command">projects</span>       View coding projects',
+    '<span class="command">history</span>        View command history',
+    '<span class="command">help</span>           You obviously already know what this does',
+    '<span class="command">email</span>          Do not email me',
+    '<span class="command">clear</span>          Clear terminal',
+    '<span class="command">banner</span>         Display the header',
+    "<br>",
+];
+
+banner = [
+    '<span class="index">4run Not A Corporation. All are connected.</span>',
+                                                                     
+"         .8.          8 888888888o.  8 8888      88 b.             8  ",
+"        .888.         8 8888    `88. 8 8888      88 888o.          8  ",
+"       :88888.        8 8888     `88 8 8888      88 Y88888o.       8  ",
+"      . `88888.       8 8888     ,88 8 8888      88 .`Y888888o.    8  ",
+"     .8. `88888.      8 8888.   ,88' 8 8888      88 8o. `Y888888o. 8  ",
+"    .8`8. `88888.     8 888888888P'  8 8888      88 8`Y8o. `Y88888o8  ",
+"   .8' `8. `88888.    8 8888`8b      8 8888      88 8   `Y8o. `Y8888  ",
+"  .8'   `8. `88888.   8 8888 `8b.    ` 8888     ,8P 8      `Y8o. `Y8  ",
+" .888888888. `88888.  8 8888   `8b.    8888   ,d8P  8         `Y8o.`  ",
+".8'       `8. `88888. 8 8888     `88.   `Y88888P'   8            `Yo  ",
+ 
+
+   
+
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
